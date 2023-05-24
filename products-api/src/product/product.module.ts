527bfddfcf10b8b53cd435a29e2aa3bbaf601875
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-24 10:48:34 -0600

    initialStructure

--- products-api/src/product/product.module.ts
+++ products-api/src/product/product.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
-@Module({})
+@Module({
+  controllers: [ProductController],
+  providers: [ProductService]
+})
 export class ProductModule {}
