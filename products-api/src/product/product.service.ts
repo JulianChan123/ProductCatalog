0f8b81c4264097b11c79d5028f19a150e8972ecb
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-25 02:54:38 -0600

    serviceCreated

--- products-api/src/product/product.service.ts
+++ products-api/src/product/product.service.ts
@@ -8,19 +8,29 @@ import { CreateProductDTO } from "./dto/product.dto";
 export class ProductService {
     constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
 
-    getProducts(){
-
+    async getProducts(): Promise<Product[]>{
+        const products = await this.productModel.find()
+        return products;
     }
 
-    getProduct(){
-
+    async getProduct(productID: string): Promise<Product>{
+        const product = await this.productModel.findById(productID);
+        return product;
     }
+
     
-    createProduct(){
-        
+    async createProduct(createProductDTO:CreateProductDTO): Promise<Product>{
+        const product = new this.productModel(createProductDTO);
+        return await product.save();
     }
 
-    updateProduct(){
-        
+    async deleteProduct(productID: string): Promise<Product>{
+        const deletedProduct = await this.productModel.findByIdAndDelete(productID);
+        return deletedProduct;
+    }
+
+    async updateProduct(productID: string, createProductDTO: CreateProductDTO): Promise<Product>{
+        const updatedProduct = await this.productModel.findByIdAndUpdate(productID, createProductDTO, {new: true});
+        return updatedProduct;
     }
 }
